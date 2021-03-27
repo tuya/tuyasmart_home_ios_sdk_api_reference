59136a8b714916bf69c6682116c0a561d07f6573
tuyaer <tuyaer@tuya.com>
2021-03-27 03:13:59 +0000

    auto generate doc

--- docs/search/searchdata.js
+++ docs/search/searchdata.js
@@ -1,27 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "_abcdefghijlmnopqrstuvwz",
-  1: "t",
-  2: "_abcdefghijlmnopqrstu",
-  3: "_abcdefghilmnopqrstuvwz",
-  4: "dt"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "classes",
-  2: "functions",
-  3: "properties",
-  4: "pages"
 };
 
 var indexSectionLabels =
 {
-  0: "All",
-  1: "Classes",
-  2: "Functions",
-  3: "Properties",
-  4: "Pages"
 };
 
