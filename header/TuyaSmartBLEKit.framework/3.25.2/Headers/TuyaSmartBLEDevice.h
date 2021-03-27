ef9c2c4143c4d7cb67dcffcf09d77fe67ffcac60
tuyaer <tuyaer@tuya.com>
2021-03-27 03:04:50 +0000

    auto generate header and doc

---
+++ header/TuyaSmartBLEKit.framework/3.25.2/Headers/TuyaSmartBLEDevice.h
@@ -0,0 +1,12 @@
+//
+// TuyaSmartBLEDevice.h
+// TuyaSmartBLEKit
+//
+// Copyright (c) 2014-2021 Tuya Inc. (https://developer.tuya.com)
+
+#import <TuyaSmartDeviceCoreKit/TuyaSmartDeviceCoreKit.h>
+
+/// @brief BLE device inherited from TuyaSmartDevice.
+@interface TuyaSmartBLEDevice : TuyaSmartDevice
+
+@end
