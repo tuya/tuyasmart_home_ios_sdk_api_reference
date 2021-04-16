e3d34d7a470743852a2c4523dbf9f7e8de6cb1ce
tuyaer <tuyaer@tuya.com>
2021-04-16 12:37:31 +0000

    auto generate header and doc

---
+++ header/TuyaSmartTimerKit.framework/Headers/TuyaSmartTimerKit.h
@@ -0,0 +1,13 @@
+//
+// TuyaSmartTimerKit.h
+// TuyaSmartTimerKit
+//
+// Copyright (c) 2014-2021 Tuya Inc. (https://developer.tuya.com)
+
+#ifndef TuyaSmartTimerKit_h
+#define TuyaSmartTimerKit_h
+
+#import "TuyaSmartTimer.h"
+#import "TuyaSmartTimer+TYDeprecatedApi.h"
+
+#endif /* TuyaSmartTimerKit_h */
